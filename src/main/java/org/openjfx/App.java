0256d5f9baee76715d8ce9185021736fc168aa6f
unknown <bradsbalogh@gmail.com>
2020-05-31 20:50:06 -0500

    added TestFX

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("dataEditor"));
+        stage.setTitle("Expense Manager");
         stage.setScene(scene);
         stage.show();
     }
--- src/test/java/org/openjfx/AppTest.java
+++ src/test/java/org/openjfx/AppTest.java
@@ -1,28 +1,52 @@
 package org.openjfx;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import org.junit.jupiter.api.DisplayName;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
 
 import static org.junit.Assert.*;
 
-public class AppTest {
+public class AppTest extends ApplicationTest {
 
+    @Override
+    public void start (Stage stage) throws Exception {
+        Parent mainNode = FXMLLoader.load(App.class.getResource("dataEditor.fxml"));
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
+
+    @Before
+    public void setUp () throws Exception {
+    }
+
+    @After
+    public void tearDown () throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
 
     @Test
-    @DisplayName("adds an item")
-    public void testAddPayment() {
-        DataEditorController dataEdit = new DataEditorController();
-
-        dataEdit.payment_name.setText("Amazon Prime");
-
-        //dataEdit.payment_price.getText();
-        //dataEdit.payment_notes.setText("");
-        //dataEdit.year_box.setValue("January");
-        //dataEdit.month_box.setValue("2020");
-
-        //System.out.println(dataEdit.payment_name);
-        //dataEdit.addPayment();
-        assertEquals(1,1);
+    public void testEnglishInput () {
+        try {Thread.sleep(1000);} catch (InterruptedException e){
+           System.out.println("ERROR");
+       }
+        //clickOn(707,284);
+        clickOn("#payment_name");
+        write("This is a test!");
     }
 }
 
