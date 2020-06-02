706a282ea7865c959a5896360f3e46170ab0f64c
Brad <bradsbalogh@gmail.com>
2020-06-01 20:01:55 -0500

    Added Tests

--- src/test/java/edu/depaul/expense/AppTest.java
+++ src/test/java/edu/depaul/expense/AppTest.java
@@ -75,7 +75,7 @@ public class AppTest extends ApplicationTest{
     }
 
     @Test
-    @DisplayName("Adds a date and payment with legal formatting")
+    @DisplayName("Tries to add a payment with illegal formatting")
     public void testAddIllegalPayment (FxRobot robot) {
         // adds month
         clickOn("#month_box");
@@ -99,6 +99,70 @@ public class AppTest extends ApplicationTest{
         sleep(500);
         // checks to see if payment was added successfully
         String stringBuild = ("Payment Price Must Be Entered As Type Double i.e. 15.25"+ '\n');
+        Assertions.assertThat(robot.lookup("#text_pane").queryAs(TextFlow.class)).hasText(stringBuild);
+        //sleep(3000);
+    }
+
+    @Test
+    @DisplayName("Tries to add a payment with no date selected")
+    public void testMissingDate (FxRobot robot) {
+        // adds payment name
+        clickOn("#payment_name");
+        String pname = "Nextlix";
+        write(pname);
+        // adds payment price
+        clickOn("#payment_price");
+        write("8.00");
+        clickOn("#add_payment");
+        sleep(500);
+        // checks to see if payment was added successfully
+        String stringBuild = ("Please Select A Date To Add Payment"+ '\n');
+        Assertions.assertThat(robot.lookup("#text_pane").queryAs(TextFlow.class)).hasText(stringBuild);
+        //sleep(3000);
+    }
+
+
+    @Test
+    @DisplayName("Tries to add payment with no payment name inputted")
+    public void testMissingPaymentName (FxRobot robot) {
+        // adds month
+        clickOn("#month_box");
+        String month = "June";
+        clickOn(month);
+        // adds year
+        clickOn("#year_box");
+        String year = "2020";
+        clickOn(year);
+        // adds payment notes
+        clickOn("#payment_notes");
+        write("Cancel Soon");
+        clickOn("#add_payment");
+        sleep(500);
+        // checks to see if payment was added successfully
+        String stringBuild = ("Payment Name And Payment Price Must Be Filled"+ '\n');
+        Assertions.assertThat(robot.lookup("#text_pane").queryAs(TextFlow.class)).hasText(stringBuild);
+        //sleep(3000);
+    }
+
+    @Test
+    @DisplayName("Tries to add payment with no payment price inputted")
+    public void testMissingPaymentPrice (FxRobot robot) {
+        // adds month
+        clickOn("#month_box");
+        String month = "June";
+        clickOn(month);
+        // adds year
+        clickOn("#year_box");
+        String year = "2020";
+        clickOn(year);
+        // adds payment name
+        clickOn("#payment_name");
+        String pname = "Amazon Prime";
+        write(pname);
+        clickOn("#add_payment");
+        sleep(500);
+        // checks to see if payment was added successfully
+        String stringBuild = ("Payment Name And Payment Price Must Be Filled"+ '\n');
         Assertions.assertThat(robot.lookup("#text_pane").queryAs(TextFlow.class)).hasText(stringBuild);
         //sleep(3000);
     }
