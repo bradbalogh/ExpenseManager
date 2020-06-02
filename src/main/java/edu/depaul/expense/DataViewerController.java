780f2e981ee85b603282da05911fffccbe086452
unknown <bradsbalogh@gmail.com>
2020-06-01 19:23:41 -0500

    Test Improvements

--- src/main/java/edu/depaul/expense/DataViewerController.java
+++ src/main/java/edu/depaul/expense/DataViewerController.java
@@ -1,5 +1,6 @@
 package edu.depaul.expense;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -25,6 +26,20 @@ public class DataViewerController {
     }
 
     @FXML
+    private void deleteFile() {
+        File file = new File("payments.csv");
+        if(file.delete())
+        {
+            System.out.println("File deleted successfully");
+        }
+        else
+        {
+            System.out.println("Failed to delete the file");
+        }
+        loadData();
+    }
+
+    @FXML
     private float getToal() {
         List<List<String>> data = DE_Controller.fileToArray();
         float total = 0;
@@ -47,29 +62,47 @@ public class DataViewerController {
         // Now loads data to page
         // Sets Total
         float total = getToal();
-        String formattedTotal = String.format("%.02f", total);
-        Text totalText = new Text("Total: $" + formattedTotal);
-        totalText.setFill(Color.WHITESMOKE);
-        totalText.setFont(Font.font("Verdana", 13));
-        total_text_pane.getChildren().add(totalText);
+        // If data file is empty
+        if(total == 0.0){
+            // Empty total
+            String formattedTotal = String.format("%.02f", total);
+            Text totalText = new Text("Total: $" + formattedTotal);
+            totalText.setFill(Color.YELLOW);
+            totalText.setFont(Font.font("Verdana", 13));
+            total_text_pane.getChildren().add(totalText);
+            // Error in text pane
+            Text paymentText = new Text("Please Add Payment Data First");
+            paymentText.setFill(Color.YELLOW);
+            paymentText.setFont(Font.font("Verdana", 13));
+            data_text_pane.getChildren().add(paymentText);
+        }
+        else {
+            String formattedTotal = String.format("%.02f", total);
+            Text totalText = new Text("Total: $" + formattedTotal);
+            totalText.setFill(Color.WHITESMOKE);
+            totalText.setFont(Font.font("Verdana", 13));
+            total_text_pane.getChildren().add(totalText);
 
-        // Loads Payment Data
-        List<List<String>> data = DE_Controller.fileToArray();
-        String note;
-        for(int i = 0; i < data.size(); i++){
-            try {
-                // checks for note
-                if(data.get(i).size()==4) {
-                    note = ("("+data.get(i).get(3)+")");
+            // Loads Payment Data
+            List<List<String>> data = DE_Controller.fileToArray();
+            String note;
+            for (int i = 0; i < data.size(); i++) {
+                try {
+                    // checks for note
+                    if (data.get(i).size() == 4) {
+                        note = ("(" + data.get(i).get(3) + ")");
+                    } else {
+                        note = "";
+                    }
+                    ;
+                    String currentText = (data.get(i).get(1) + note + " - $" + data.get(i).get(2) + " [" + data.get(i).get(0) + "]");
+                    Text paymentText = new Text(currentText + '\n');
+                    paymentText.setFill(Color.WHITESMOKE);
+                    paymentText.setFont(Font.font("Verdana", 13));
+                    data_text_pane.getChildren().add(paymentText);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Skipping Empty Row...");
                 }
-                else {note = "";};
-                String currentText = (data.get(i).get(1) + note + " - $" + data.get(i).get(2) + " [" + data.get(i).get(0) + "]");
-                Text paymentText = new Text(currentText + '\n');
-                paymentText.setFill(Color.WHITESMOKE);
-                paymentText.setFont(Font.font("Verdana", 13));
-                data_text_pane.getChildren().add(paymentText);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Skipping Empty Row...");
             }
         }
     }
--- src/test/java/edu/depaul/expense/AppTest.java
+++ src/test/java/edu/depaul/expense/AppTest.java
@@ -4,16 +4,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.testfx.assertions.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxToolkit;
+import org.testfx.api.FxRobot;
+import javafx.scene.control.Button;
+import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.Start;
 
-public class AppTest extends ApplicationTest {
+import javax.swing.*;
+
+@ExtendWith(ApplicationExtension.class)
+public class AppTest extends ApplicationTest{
 
     @Override
     public void start (Stage stage) throws Exception {
@@ -35,17 +46,20 @@ public class AppTest extends ApplicationTest {
     }
 
     @Test
-    @DisplayName("Adds a date adn payment with legal formatting")
-    public void testAddLegalPayment () {
+    @DisplayName("Adds a date and payment with legal formatting")
+    public void testAddLegalPayment (FxRobot robot) {
         // adds month
         clickOn("#month_box");
-        type(KeyCode.ENTER);
+        String month = "June";
+        clickOn(month);
         // adds year
         clickOn("#year_box");
-        type(KeyCode.ENTER);
+        String year = "2020";
+        clickOn(year);
         // adds payment name
         clickOn("#payment_name");
-        write("Amazon Prime");
+        String pname = "Amazon Prime";
+        write(pname);
         // adds payment price
         clickOn("#payment_price");
         write("5.00");
@@ -53,8 +67,40 @@ public class AppTest extends ApplicationTest {
         clickOn("#payment_notes");
         write("Cancel Soon");
         clickOn("#add_payment");
-        try {Thread.sleep(500);} catch (InterruptedException e) {}
+        sleep(500);
+        // checks to see if payment was added successfully
+        String stringBuild = ("Added Payment: "+pname+" ["+month+year+"]\n");
+        Assertions.assertThat(robot.lookup("#text_pane").queryAs(TextFlow.class)).hasText(stringBuild);
+        //sleep(3000);
+    }
 
+    @Test
+    @DisplayName("Adds a date and payment with legal formatting")
+    public void testAddIllegalPayment (FxRobot robot) {
+        // adds month
+        clickOn("#month_box");
+        String month = "June";
+        clickOn(month);
+        // adds year
+        clickOn("#year_box");
+        String year = "2020";
+        clickOn(year);
+        // adds payment name
+        clickOn("#payment_name");
+        String pname = "Nextlix";
+        write(pname);
+        // adds payment price
+        clickOn("#payment_price");
+        write("$8.00");
+        // adds payment notes
+        clickOn("#payment_notes");
+        write("Cancel Soon");
+        clickOn("#add_payment");
+        sleep(500);
+        // checks to see if payment was added successfully
+        String stringBuild = ("Payment Price Must Be Entered As Type Double i.e. 15.25"+ '\n');
+        Assertions.assertThat(robot.lookup("#text_pane").queryAs(TextFlow.class)).hasText(stringBuild);
+        //sleep(3000);
     }
 }
 
